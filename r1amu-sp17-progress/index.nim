5b964aed976ce190013ab00a9e704bcf9cda2bf2
jqoh <65055928+hand-accident@users.noreply.github.com>
2022-12-21 19:38:27 +0900

    VComponentをやめたことでDiffingしても動くようになった

--- r1amu-sp17-progress/index.nim
+++ r1amu-sp17-progress/index.nim
@@ -3,7 +3,6 @@ include karax/prelude
 import view
 
 proc createDom: VNode =
-  kxi.avoidDomDiffing
   result = buildHtml(tdiv):
     navBar()
     displaycharts()
--- r1amu-sp17-progress/view.nim
+++ r1amu-sp17-progress/view.nim
@@ -5,44 +5,25 @@ import std/[algorithm, sequtils, sugar, strformat, strutils, options]
 include karax/prelude
 import chartdata, stateutils
 const eK = kstring""
-type
-  Card = ref object of VComponent
-    header, content, actions: VNode
-  ChartCard = ref object of VComponent
-    chart: ChartInfo
-  Toggle* = ref object of VComponent
-    checked*: bool
-  SortButton = ref object of VComponent
-    order: ChartSortOrder
-  NavBar = ref object of VComponent
-    cts: array[AchievementEnum, Toggle]
-    vts: array[AvailabilityEnum, Toggle]
-    bs: array[ChartSortOrder, SortButton]
+
 proc card(
     header, content, actions: VNode,
-    classes = (ek, ek, ek, ek)): Card =
-  proc render(x: VComponent): VNode =
-    let self = Card(x)
-    result = buildHtml:
-      tdiv(class = fmt"card shadow-xl {classes[0]}"):
-        tdiv(class = fmt"card-title {classes[1]}"):
-          self.header
-        tdiv(class = fmt"card-body {classes[2]}"):
-          self.content
-          tdiv(class = fmt"card-actions {classes[3]}"):
-            self.actions
-  result = Card.newComponent render
-  result.header = header
-  result.content = content
-  result.actions = actions
+    classes = (ek, ek, ek, ek)): VNode =
+  result = buildHtml:
+    tdiv(class = fmt"card shadow-xl {classes[0]}"):
+      tdiv(class = fmt"card-title {classes[1]}"):
+        header
+      tdiv(class = fmt"card-body {classes[2]}"):
+        content
+        tdiv(class = fmt"card-actions {classes[3]}"):
+          actions
 
-proc chartCard(c: ChartInfo): ChartCard =
-  proc render(x: VComponent): VNode =
-    let hyphen = " - "
-    let self = ChartCard(x)
-    let header = buildHTML(tdiv(class = "flex flex-row")):
-      p(self.chart.name.text)
-      case self.chart.kind
+proc chartCard(c: ChartInfo): VNode =
+  let
+    hyphen = " - "
+    header = buildHTML(tdiv(class = "flex flex-row")):
+      p(c.name.text)
+      case c.kind
       of Beginner:
         tdiv(class = "badge badge-info badge-outline", text"習")
       of Basic:
@@ -53,18 +34,16 @@ proc chartCard(c: ChartInfo): ChartCard =
         tdiv(class = "badge badge-success badge-outline", text"激")
       of Challenge:
         tdiv(class = "badge badge-primary badge-outline", text"鬼")
-    let content = buildHTML(tdiv):
-      p(class = "text-3xl", self.chart.displayScore.text)
-      p(class = "text-xl", fmt"BPM: {self.chart.bpm.main}".text)
-      p(class = "text-xs", self.chart.bpm.sub.join(hyphen).text)
-    let actions = tdiv.buildHTML:
-      tdiv(class = "badge badge-outline", self.chart.displayClearDay.text)
-      tdiv(class = "badge badge-outline", self.chart.displayState.text)
-    result = buildHtml(section(id = fmt"chart-{self.chart.name}-{self.chart.kind}")):
-      card(header, content, actions, (kstring"w-96 m-4", ek, ek,
-          kstring"justify-end"))
-  result = ChartCard.newComponent render
-  result.chart = c
+    content = buildHTML(tdiv):
+      p(class = "text-3xl", c.displayScore.text)
+      p(class = "text-xl", fmt"BPM: {c.bpm.main}".text)
+      p(class = "text-xs", c.bpm.sub.join(hyphen).text)
+    actions = tdiv.buildHTML:
+      tdiv(class = "badge badge-outline", c.displayClearDay.text)
+      tdiv(class = "badge badge-outline", c.displayState.text)
+  result = buildHtml(section(id = fmt"chart-{c.name}-{c.kind}")):
+    card(header, content, actions, (kstring"w-96 m-4", ek, ek,
+        kstring"justify-end"))
 
 proc displaycharts*(chartIndices: seq[int]): VNode =
   result = buildHtml(tdiv(class = "flex flex-wrap")):
@@ -76,18 +55,14 @@ proc displaycharts*(chartIndices: seq[int]): VNode =
           button(class = "btn btn-primary btn-lg"):
             text"back to top"
 
-proc newToggle[T](name: string, checked: bool, val: T): Toggle =
-  proc render(x: VComponent): VNode =
-    let self = Toggle(x)
-    result = buildHtml(tdiv(class = "p-4")):
-      tdiv(class = "indicator"):
-        span(class = "indicator-item indicator-start badge"):
-          text(name)
-        input(`type` = "checkbox", class = "toggle toggle-primary toggle-lg",
-            checked = self.checked.toChecked, onclick = () => (
-                state = state.changeState(val)))
-  result = Toggle.newComponent render
-  result.checked = checked
+proc newToggle[T](name: string, checked: bool, val: T): VNode =
+  result = buildHtml(tdiv(class = "p-4")):
+    tdiv(class = "indicator"):
+      span(class = "indicator-item indicator-start badge"):
+        text(name)
+      input(`type` = "checkbox", class = "toggle toggle-primary toggle-lg",
+          checked = checked.toChecked, onclick = () => (
+              state = state.changeState(val)))
 
 proc buttonColor(cond: bool): string =
   if cond:
@@ -95,44 +70,28 @@ proc buttonColor(cond: bool): string =
   else:
     "btn-secondary"
 
-proc sortButton(name: string, order, sOrder: ChartSortOrder): SortButton =
-  proc render(x: VComponent): VNode =
-    let self = SortButton(x)
-    result = buildHtml(tdiv(class = "p-4")):
+proc sortButton(name: string, order, sOrder: ChartSortOrder): VNode =
+  result = buildHtml(tdiv(class = "p-4")):
+    button(class = fmt"btn {buttonColor(order == sOrder)}"):
+      proc onclick = state = state.changeState order
+      text(name)
 
-      button(class = fmt"btn {buttonColor(self.order == sOrder)}"):
-        proc onclick = state = state.changeState order
-        text(name)
-  result = SortButton.newComponent render
-  result.order = order
+proc navBar*(prms: BtnPrms): VNode =
+  result = buildHtml(tdiv(class = "sticky")):
+    tdiv(class = "navbar bg-base-200"):
+      ul(class = "menu menu-horizontal px-1"):
+        for (c, name) in AchievementEnum.toSeq.zip(@["cleared", "failed", "notPlayed"]):
+          li:
+            newToggle(name, prms.dispKind.achievement[c], c)
+        for (v, name) in AvailabilityEnum.toSeq.zip(@["playable", "deleted", "locked"]):
+          li:
+            newToggle(name, prms.dispKind.availability[v], v)
+        for (cso, name) in ChartSortOrder.toSeq.zip(@["曲名", "スコア",
+            "クリア日"]):
+          li:
+            sortButton(name, cso, prms.order)
 
-proc navBar*(prms: BtnPrms): NavBar =
-  proc render(x: VComponent): VNode =
-    let self = NavBar(x)
-    result = buildHtml(tdiv(class = "sticky")):
-      tdiv(class = "navbar bg-base-200"):
-        ul(class = "menu menu-horizontal px-1"):
-          for cToggle in self.cts:
-            li:
-              cToggle
-          for vToggle in self.vts:
-            li:
-              vToggle
-          for b in self.bs:
-            li:
-              b
-
-  result = NavBar.newComponent render
-  for (c, name) in AchievementEnum.toSeq.zip(@["cleared", "failed",
-      "notPlayed"]):
-    result.cts[c] = newToggle(name, prms.dispKind.achievement[c], c)
-  for (v, name) in AvailabilityEnum.toSeq.zip(@["playable", "deleted", "locked"]):
-    result.vts[v] = newToggle(name, prms.dispKind.availability[v], v)
-  for (cso, name) in ChartSortOrder.toSeq.zip(@["曲名", "スコア",
-      "クリア日"]):
-    result.bs[cso] = sortButton(name, cso, prms.order)
-
-proc navBar*: NavBar =
+proc navBar*: VNode =
   state.btns.navBar
 
 proc displaycharts*: VNode =
